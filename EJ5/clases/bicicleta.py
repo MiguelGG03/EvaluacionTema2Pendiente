6c5f78481ae00f3e21807e564add3bc74280289a
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-10-07 16:39:55 +0200

    cambios en las clases

--- EJ5/clases/bicicleta.py
+++ EJ5/clases/bicicleta.py
@@ -2,7 +2,7 @@ from vehiculo import Vehiculo
 
 class Bicicleta(Vehiculo):
 
-    def __init__(self, color, ruedas,tipo):
+    def __init__(self, color, ruedas, tipo):
         Vehiculo.__init__(self, color, ruedas)
         self.tipo = tipo
 
--- EJ5/main5.py
+++ EJ5/main5.py
@@ -7,8 +7,21 @@ from clases.coche import Coche
 
 def main():
 
-    coche=Vehiculo('Rojo',4)
-    print(coche.__str__())
+    vehiculo=Vehiculo('Rojo',4)
+    moto=Motocicleta('Rojo',2,'Deportiva',240,15)
+    camion=Camioneta('Rojo',4,150,1200,1500)
+    print(camion.__str__())
+    bici=Bicicleta('Rojo',2,'Deportiva')
+    coche=Coche('Rojo',4,150,1200)
+    
+    print(f'Vehiculo: {vehiculo.__str__()}\nCoche: {coche.__str__()}\n Bicicleta: {bici.__str__()}\nCamioneta: {camion.__str__()}\nMotocicleta: {moto.__str__()}')
+    lista=[]
+    lista.append(vehiculo)
+    lista.append(moto)
+    lista.append(camion)
+    lista.append(bici)
+    lista.append(coche)
+
 
 if __name__=='__main__':
     main()
