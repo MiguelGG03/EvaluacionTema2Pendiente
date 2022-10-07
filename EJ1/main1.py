afd67252e4f250c77b3175ceb6980a4202d1bc19
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-10-07 16:54:19 +0200

    cambios en el main

--- EJ1/main1.py
+++ EJ1/main1.py
@@ -1,3 +1,17 @@
-from clases.Alumno import 
+from clases.Alumno import Alumno
 
 def main1():
+    a1=Alumno('Diego',8)
+    a2=Alumno('Alberto',9)
+    a3=Alumno('Javier',3)
+    a4=Alumno('Luis',-12)
+    a5=Alumno('David',13)
+    a1.__str__()
+    a2.__str__()
+    a3.__str__()
+    a4.__str__()
+    a5.__str__()
+
+
+if __name__=='__main__':
+    main1()
